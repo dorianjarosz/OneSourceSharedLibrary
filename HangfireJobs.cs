67bdb2197c2e48c1cee3d61248126c48f319a051
Dorian Jarosz <dorian.jarosz@eviden.com>
2024-06-26 13:41:36 +0200

    Init

---
+++ HangfireJobs.cs
@@ -0,0 +1,10 @@
+﻿namespace OneSourceSharedLibrary
+{
+    public class HangfireJobs
+    {
+        public async Task ExecuteRecurringJobAsync(Task recurringJob)
+        {
+            await recurringJob;
+        }
+    }
+}
